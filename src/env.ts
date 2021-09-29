921a8de89081cf5b788eb13a365ef5bc59a2c962
Gourisankar Das <goudas@redhat.com>
2021-09-29 12:36:12 +0530

    changed port from 3001 to 3000

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,6 @@ export const serviceUrl = 'http://unchat.online';
 // export const serviceUrl = 'http://localhost:8888';
 
 export const clientUrl = 'http://vlur.online';
-// export const clientUrl = 'http://localhost:3001';
+// export const clientUrl = 'http://localhost:3000';
 
 export const inProduction = false;
